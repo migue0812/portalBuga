a3eba2cdee8d891e34c2eb51683db9d8105eb702
David <David@Davidim>
2016-04-27 12:46:38 -0500

    Panel de control

---
+++ resources/views/Templates/Backend/headPanel.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+
+    <title>Portal buga</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('panel/css/bootstrap.css')}}" rel="stylesheet">
+    <!--external css-->
+    <link href="{{asset('panel/font-awesome/css/font-awesome.css')}}" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="{{asset('panel/css/zabuto_calendar.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('panel/js/gritter/css/jquery.gritter.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{asset('panel/lineicons/style.css')}}">    
+
+    <!-- Custom styles for this template -->
+    <link href="{{asset('panel/css/style.css')}}" rel="stylesheet">
+    <link href="{{asset('panel/css/style-responsive.css')}}" rel="stylesheet">
+
+    <script src="{{asset('panel/js/chart-master/Chart.js')}}"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
