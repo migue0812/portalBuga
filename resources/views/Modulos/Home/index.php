fec6610416b96e99d75d2790fa86edcf870eae9a
Ne_K Music <Ne_K Music@Ne_K.mshome.net>
2016-04-27 13:09:20 +0000

    Index Full

--- resources/views/Modulos/Home/index.php
+++ resources/views/Modulos/Home/index.php
@@ -92,6 +92,60 @@
         </div>
     </div>
 </div>
+<div class="container what">
+    <div class="row ">
+        <div class="box">
+            <div class="queHacer">
+                <div>
+                    <h2 style="text-align: center;">Que Hacer</h2>
+                </div>
+                <hr class="tagline-divider-sin">
+            </div>
+            <!-- Page Content -->
+            <div class="container">
+
+                <!-- Introduction Row -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">Visitanos
+                            <small>Los Mejores Lugares Para Ti !</small>
+                        </h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, explicabo dolores ipsam aliquam inventore corrupti eveniet quisquam quod totam laudantium repudiandae obcaecati ea consectetur debitis velit facere nisi expedita vel?</p>
+                    </div>
+                </div>
+
+                <!-- Team Members Row -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h2 class="page-header"></h2>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 text-center">
+                        <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                        <h3>John Smith
+                            <small>Job Title</small>
+                        </h3>
+                        <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 text-center">
+                        <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                        <h3>John Smith
+                            <small>Job Title</small>
+                        </h3>
+                        <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 text-center">
+                        <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                        <h3>John Smith
+                            <small>Job Title</small>
+                        </h3>
+                        <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.container -->
+    </div>
+</div>
 <!-- Script to Activate the Carousel -->
 <script>
     $('.carousel').carousel({
