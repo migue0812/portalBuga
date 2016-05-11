f194787fba5c5b9bfe1d2cb07a3fe52f42ee4af1
LIZ <LIZ@LIZETH>
2016-05-11 15:50:44 -0500

    REGISTRO DE USUARIO (panel)

--- app/Http/Controllers/Modulos/Panel/ConfigurarController.php
+++ app/Http/Controllers/Modulos/Panel/ConfigurarController.php
@@ -24,9 +24,32 @@ class ConfigurarController extends Controller {
   }
   
   function getCrear(Request $request) {
+      $dato_usuario = DB::select("SELECT * FROM bdp_dato_usuario");
     return view("Modulos.Panel.configurar.crear");
   }
   
+  function postCrear(Request $request) {
+       $dus_nombre = $_POST['dus_nombre'];
+       $dus_apellidos = $_POST['dus_apellidos'];
+       $dus_correo = $_POST['dus_correo'];
+       $dus_genero = $_POST['dus_genero'];
+       $dus_fecha_nacimiento = $_POST['dus_fecha_nacimiento'];
+       $dus_facebook = $_POST['dus_facebook'];
+       $dus_twitter = $_POST['dus_twitter'];
+       $dus_google_plus = $_POST['dus_google_plus'];
+       //$dus_avatar = $_FILES['dus_avatar']["name"]; 
+       //$ruta = $_FILES["dus_avatar"]["tmp_name"];
+       //$destino = "img/". $dus_avatar;
+       //copy($ruta, $destino);
+       
+       DB::insert("INSERT INTO bdp_dato_usuario (dus_id,usu_id,dus_nombre,dus_apellido,dus_correo,dus_genero,dus_fecha_nacimiento,dus_facebook,dus_twitter,dus_google_plus) "
+              . "VALUES(?,?,?,?,?,?,?,?,?,?)", 
+              array($dus_nombre, $dus_apellidos, $dus_correo, $dus_genero, $dus_fecha_nacimiento, $dus_facebook, $dus_twitter, $dus_google_plus));
+       
+       
+      
+  }
+          
   function getEditar(Request $request) {
     return view("Modulos.Panel.configurar.editar");
   }
