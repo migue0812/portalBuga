f98bedafc4e3fb25430889050d56ac22548fc422
Diego Alexis Alzate <Diego Alexis Alzate@DiegoAlexis>
2016-05-13 06:10:44 -0500

    PORTAFOLIO :HOVER index + Slider Informacion index +  vista grande imagen del sitio efecto hover

--- resources/views/Modulos/Home/sitios.php
+++ resources/views/Modulos/Home/sitios.php
@@ -1,17 +1,13 @@
 <?php include ("/../../Templates/Frontend/head.php") ?>
 <?php include ("/../../Templates/Frontend/header.php") ?>
-<!-- www.TuTiempo.net - Ancho:218px - Alto:72px -->
-<div id="TT_txtgLBYhdUUaDchUNAwDjzzjD9lUMacFbtEdEsCoaED"><a href="http://www.tutiempo.net/buga.html">Buga</a></div>
-<script type="text/javascript" src="http://www.tutiempo.net/widget/eltiempo_txtgLBYhdUUaDchUNAwDjzzjD9lUMacFbtEdEsCoaED"></script>
 
 <div class="container">
     <div class="row">
         <div class="box">
-            <hr id='hrAlz'>
             <div class="visitNav">
                 <div id="alz"><h3>SITIOS TURISTICOS DE BUGA</h3></div>
             </div>
-            <hr id='hrAlz'>
+            <hr class="tagline-divider-cont">
             <div class="container portImg">
                 <div class="row ">
                     <div class="col-md-6 portfolio-item">
@@ -22,10 +18,11 @@
                     </div>
                     <div class="col-md-5 portfolio-item">
                         <h2 style="text-align: center">Estacion Ferrea</h2>
-                        <hr class="hrWhat">
+                       <hr class="tagline-divider-cont">
                         <p class="text-justify">Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Alex Campos, reconocido en el mundo de la música cristiana.. Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Alex Campos, reconocido en e Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Ale
                             Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Al </p>
-                        <a href="<?php echo url("home/sitios/det") ?>" class="btnalz">Ver Más</a>
+                        <a href="<?php echo url("home/sitios/det") ?>" class="btn btn-primary"><b title="Ver mas"class="icon-plus fa-fw"></b></a>
+               <a href="<?php echo url("itinerario/sitios") ?>" class="btn btn-primary"><b title="Itinerario" class="icon-direction fa-fw"></b></a>
                     </div>
                 </div>
             </div>
@@ -49,10 +46,11 @@
                     </div>
                     <div class="col-md-5 portfolio-item">
                         <h2 style="text-align: center">Basilica de Buga</h2>
-                        <hr class="hrWhat">
+                         <hr class="tagline-divider-cont">
                         <p class="text-justify">Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Alex Campos, reconocido en el mundo de la música cristiana.. Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Alex Campos, reconocido en e Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Ale
                             Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Al </p>
-                        <a href="#" class="btnalz">Ver Más</a>
+                        <a href="<?php echo url("home/sitios/det") ?>" class="btn btn-primary"><b title="Ver mas"class="icon-plus fa-fw"></b></a>
+               <a href="<?php echo url("itinerario/sitios") ?>" class="btn btn-primary"><b title="Itinerario" class="icon-direction fa-fw"></b></a>
                     </div>
                 </div>
             </div>
@@ -63,18 +61,18 @@
 <div class="container">
     <div class="row">
         <div class="box">
-            <hr id='hrAlz'>
             <div class="visitNav">
-                <div id="alz"><h3>SITIOS MAS VISITADOS</h3></div>
+                <div id="alz"><h3>SITIOS TURISTICOS DE BUGA</h3></div>
             </div>
-            <hr id='hrAlz'>
+            <hr class="tagline-divider-cont">
             <div class="col-md-4 portfolio-item">
                 <div id="tituloalz"><h2>Bioparque el Vergel</h2></div>
                 <a href="#">
                     <img class="img-responsive" src="<?php echo asset("img/Holmes.png") ?>">
                 </a>        
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae...</p>
-                <a href="#" class="btnalz">Ver Más</a>
+                <a href="<?php echo url("home/sitios/det") ?>" class="btn btn-primary"><b title="Ver mas"class="icon-plus fa-fw"></b></a>
+               <a href="<?php echo url("itinerario/sitios") ?>" class="btn btn-primary"><b title="Itinerario" class="icon-direction fa-fw"></b></a>
             </div>  
 
 
@@ -84,21 +82,20 @@
                     <img class="img-responsive" src="<?php echo asset("img/Holmes.png") ?>">
                 </a>        
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae...</p>
-                <a href="#" class="btnalz">Ver Más</a>
+                <a href="<?php echo url("home/sitios/det") ?>" class="btn btn-primary"><b title="Ver mas"class="icon-plus fa-fw"></b></a>
+               <a href="<?php echo url("itinerario/sitios") ?>" class="btn btn-primary"><b title="Itinerario" class="icon-direction fa-fw"></b></a>
             </div> 
 
 
             <div class="col-md-4 portfolio-item">
-                <div id="tituloalz"><h2>La Maria</h2></div>
+                <div id="tituloalz"><h2>La María</h2></div>
                 <a href="#">
                     <img class="img-responsive" src="<?php echo asset("img/Holmes.png") ?>">
                 </a>        
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae...</p>
-                <a href="#" class="btnalz">Ver Más</a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravidas pellentesque urna varius vitae...</p>
+                <a href="<?php echo url("home/sitios/det") ?>" class="btn btn-primary"><b title="Ver mas"class="icon-plus fa-fw"></b></a>
+               <a href="<?php echo url("itinerario/sitios") ?>" class="btn btn-primary"><b title="Itinerario" class="icon-direction fa-fw"></b></a>
             </div> 
-        </div>
-    </div>
-</div>
-
+        
 <?php include ("/../../Templates/Frontend/footer.php") ?>
 <?php include ("/../../Templates/Frontend/foot.php") ?>
