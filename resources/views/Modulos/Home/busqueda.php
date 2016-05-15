63dbecb7033b8184fc68d5f8d6b040cf20df9248
Balem <Balem@Balem.mshome.net>
2016-05-15 13:51:43 -0500

    Search Modif

--- resources/views/Modulos/Home/busqueda.php
+++ resources/views/Modulos/Home/busqueda.php
@@ -6,50 +6,108 @@
             <div class="col-lg-12">
                 <div class="visitNav">
                     <div>
-                        <h2 style="text-align: center;">Busqueda</h2>
+                        <h2 style="text-align: center;">Resultados De Tu Busqueda</h2>
                     </div>
                 </div>
                 <hr class="tagline-divider-cont">
             </div>
-            <div class="portafolio">
-                <div class="col-sm-4 text-center">
-                    <img class="img-responsive" src="http://placehold.it/750x450" alt="">
-                    <h3>John Smith
-                        <small>Job Title</small>
-                    </h3>
-                </div>
-                <div class="col-sm-4 text-center">
-                    <img class="img-responsive" src="http://placehold.it/750x450" alt="">
-                    <h3>John Smith
-                        <small>Job Title</small>
-                    </h3>
-                </div>
-                <div class="col-sm-4 text-center">
-                    <img class="img-responsive" src="http://placehold.it/750x450" alt="">
-                    <h3>John Smith
-                        <small>Job Title</small>
-                    </h3>
-                </div>
-                <div class="col-sm-4 text-center">
-                    <img class="img-responsive" src="http://placehold.it/750x450" alt="">
-                    <h3>John Smith
-                        <small>Job Title</small>
-                    </h3>
-                </div>
-                <div class="col-sm-4 text-center">
-                    <img class="img-responsive" src="http://placehold.it/750x450" alt="">
-                    <h3>John Smith
-                        <small>Job Title</small>
-                    </h3>
-                </div>
-                <div class="col-sm-4 text-center">
-                    <img class="img-responsive" src="http://placehold.it/750x450" alt="">
-                    <h3>John Smith
-                        <small>Job Title</small>
-                    </h3>
-                </div>
-                <div class="clearfix"></div>
+            <div class="col-md-4 text-center">
+                <article class="articulo">    
+                    <a>
+                        <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                    </a>
+                    <h2 class="titulo-evento">
+                        <a href="#"></a>
+                    </h2>
+                    <p><span class="articulofecha"></span><span class="articulo-autor"><a href="#"></a></span></p>
+                    <p class="articulo-contenido">
+                    </p>
+                    <div class="contenedor-botones">
+                        <a href="#" class="btn btn-primary">Ver Mas</a>
+                    </div>
+                </article>
             </div>
+            <div class="col-md-4 text-center">
+                <article class="articulo">    
+                    <a>
+                        <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                    </a>
+                    <h2 class="titulo-evento">
+                        <a href="#"></a>
+                    </h2>
+                    <p><span class="articulofecha"></span><span class="articulo-autor"><a href="#"></a></span></p>
+                    <p class="articulo-contenido">
+                    </p>
+                    <div class="contenedor-botones">
+                        <a href="#" class="btn btn-primary">Ver Mas</a>
+                    </div>
+                </article>
+            </div>
+            <div class="col-md-4 text-center">
+                <article class="articulo">    
+                    <a>
+                        <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                    </a>
+                    <h2 class="titulo-evento">
+                        <a href="#"></a>
+                    </h2>
+                    <p><span class="articulofecha"></span><span class="articulo-autor"><a href="#"></a></span></p>
+                    <p class="articulo-contenido">
+                    </p>
+                    <div class="contenedor-botones">
+                        <a href="#" class="btn btn-primary">Ver Mas</a>
+                    </div>
+                </article>
+            </div>
+            <div class="col-md-4 text-center">
+                <article class="articulo">    
+                    <a>
+                        <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                    </a>
+                    <h2 class="titulo-evento">
+                        <a href="#"></a>
+                    </h2>
+                    <p><span class="articulofecha"></span><span class="articulo-autor"><a href="#"></a></span></p>
+                    <p class="articulo-contenido">
+                    </p>
+                    <div class="contenedor-botones">
+                        <a href="#" class="btn btn-primary">Ver Mas</a>
+                    </div>
+                </article>
+            </div>
+            <div class="col-md-4 text-center">
+                <article class="articulo">    
+                    <a>
+                        <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                    </a>
+                    <h2 class="titulo-evento">
+                        <a href="#"></a>
+                    </h2>
+                    <p><span class="articulofecha"></span><span class="articulo-autor"><a href="#"></a></span></p>
+                    <p class="articulo-contenido">
+                    </p>
+                    <div class="contenedor-botones">
+                        <a href="#" class="btn btn-primary">Ver Mas</a>
+                    </div>
+                </article>
+            </div>
+            <div class="col-md-4 text-center">
+                <article class="articulo">    
+                    <a>
+                        <img class="img-responsive" src="http://placehold.it/750x450" alt="">
+                    </a>
+                    <h2 class="titulo-evento">
+                        <a href="#"></a>
+                    </h2>
+                    <p><span class="articulofecha"></span><span class="articulo-autor"><a href="#"></a></span></p>
+                    <p class="articulo-contenido">
+                    </p>
+                    <div class="contenedor-botones">
+                        <a href="#" class="btn btn-primary">Ver Mas</a>
+                    </div>
+                </article>
+            </div>
+
             <div class="col-lg-12">
                 <nav>
                     <div class="text-center">
@@ -73,6 +131,7 @@
                     </div>
                 </nav>
             </div>
+
 
             <?php include ("/../../Templates/Frontend/footer.php") ?>
             <?php include ("/../../Templates/Frontend/foot.php") ?> 
