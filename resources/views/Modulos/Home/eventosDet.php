846c9dfeb775ba3f97ffaa17898a87883f2521d5
EstebanCuadros <EstebanCuadros@Esteban.connectify>
2016-05-17 16:44:39 -0500

    Vista evento detalles

--- resources/views/Modulos/Home/eventosDet.php
+++ resources/views/Modulos/Home/eventosDet.php
@@ -18,45 +18,44 @@
 
             <div class="col-md-6 portfolio-item">
                 <a href="se devuleve al index">
-                    <img class="img-responsive" src="<?php echo asset("img/ferias.jpg") ?>" alt="">
-
-
+                    <img class="img-responsive" src="<?php echo asset("$eveDetalle->img_ruta") ?>" alt="">
                 </a>
 
             </div>
             <div class="botoness">
                 <div class="col-md-2 portfolio-item">
-                    <h3 title="Lugar"class="icon-home fa-fw">:</h3>
-                    <p>Coliseo de ferias</p>
-                </div>
-                <div class="col-md-2 portfolio-item">
                     <h3 title="Direccion" class="icon-location fa-fw"><b>:</b></h3>
-                    <p>Carrera 8 con calle 21</p>
+                    <p><?php echo("$eveDetalle->eve_direccion") ?></p>
                 </div>
                 <div class="col-md-2 portfolio-item">
-                    <h3 title="Duraccion" class=" icon-calendar fa-fw"><b>:</b></h3><p>Del 15 de julio hasta el 20 de julio</p>
+                    <h3 title="Duraccion" class=" icon-calendar fa-fw"><b>:</b></h3><p>Del <?php echo("$eveDetalle->eve_fecha_inicio") ?> hasta el <?php echo("$eveDetalle->eve_fecha_fin") ?></p>
                 </div>
                 <div class="col-md-2 portfolio-item">
-                    <h3 title="Correo" class=" icon-"><b>:</b></h3>
-                    <p>$30.0000</p>
+                    <h3 title="Valor" class=" icon-usd fa-fw"><b>:</b></h3>
+                    <p><?php echo("$eveDetalle->eve_valor_boleta") ?></p>
                 </div>
                 <div class="col-md-2 portfolio-item">
                     <h3 title="Celular" class="icon-mobile"><b>:</b></h3>
-                    <p>3005647843</p>
+                    <p><?php echo("$eveDetalle->eve_telefono_contacto") ?></p>
                 </div>
                 <div class="col-md-2 portfolio-item">
                     <h3 title="Hora" class="icon-clock"><b>:</b></h3>
-                    <p>De2pm a 7pm</p>
+                    <p><?php echo("$eveDetalle->eve_fecha_hora") ?></p>
                 </div>
-                <div class="col-md-3 portfolio-item">
-                    <h3 title="Correo" class=" icon-mail fa-fw"><b>:</b></h3><p>AlcaldiaDeBuga@hotmail.com</p>
+                <div class="col-md-2 portfolio-item">
+                    <h3 title="Correo" class=" icon-mail fa-fw"><b>:</b></h3>
+                    <p><?php echo("$eveDetalle->eve_correo_contacto") ?></p>
                 </div>
             </div>
             <div class="col-md-5 col-sm-11 portfolio-item">
-                <h2 style="margin-top: 60px;">Ferias De Buga</h2>
-                <p style="margin-top:20px; "class="text-justify">Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Alex Campos, reconocido en el mundo de la música cristiana.. Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Alex Campos, reconocido en e Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Ale
-                    Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Al </p>
+                <h2 style="margin-top: 60px;"><?php echo("$eveDetalle->eve_nombre") ?></h2>
+                <p style="margin-top:20px; "class="text-justify"><?php echo("$eveDetalle->eve_descripcion") ?></p>
             </div>
+        </div>
+    </div>
+
+   <div class="row">
+        <div class="box">
             <div id="fb-root"></div>
             <script>(function (d, s, id) {
                     var js, fjs = d.getElementsByTagName(s)[0];
@@ -69,6 +68,14 @@
                 }(document, 'script', 'facebook-jssdk'));</script>
 
             <div class="fb-comments" data-href="https://www.facebook.com/groups/860979787324031/?fref=ts" data-numposts="5"></div>
-
+</div>
+    </div>
+   <div class="row">
+        <div class="box">
             <?php include ("/../../Templates/Frontend/footer.php") ?>
             <?php include ("/../../Templates/Frontend/foot.php") ?>
+
+    </div>
+    </div>
+</div>        
+            
