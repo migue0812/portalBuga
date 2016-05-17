4667cd610628bb833ed94b4f99911d0352ced00a
Ne_K Music <Ne_K Music@Ne_K>
2016-05-17 18:43:10 +0000

    Icon Event

--- resources/views/Modulos/Home/eventosDet.php
+++ resources/views/Modulos/Home/eventosDet.php
@@ -14,43 +14,61 @@
             </div>
             <hr class="tagline-divider-cont">
 
-         <!-- Projects Row -->
-         
-                    <div class="col-md-6 portfolio-item">
-                        <a href="se devuleve al index">
-                            <img class="img-responsive" src="<?php echo asset("img/ferias.jpg") ?>" alt="">
+            <!-- Projects Row -->
 
-                           
-                        </a>
-                    </div>
-                    <div class="col-md-1 portfolio-item">
-                        <h4 class=" icon-home"><b>:</b></h4>
-                        <p>Coliseo de ferias</p>
-                    </div>
-                    <div class="col-md-2 portfolio-item">
-                        <h4 class="icon-location"><b>:</b></h4>
-                        <p>Carrera 8 con calle 21</p>
-                    </div>
-                    <div class="col-md-2 portfolio-item">
-                        <h4 class=" icon-calendar"><b>:</b></h4><p>Del 15 de julio hasta el 20 de julio</p>
-                    </div>
-                    <div class="col-md-5 col-sm-11 portfolio-item">
-                        <h2 style="margin-top: 60px;">Ferias De Buga</h2>
-                        <p class="text-justify">Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Alex Campos, reconocido en el mundo de la música cristiana.. Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Alex Campos, reconocido en e Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Ale
-                            Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Al </p>
-                    </div>     
-                                <div id="fb-root"></div>
-                                <script>(function(d, s, id) {
-                                        var js, fjs = d.getElementsByTagName(s)[0];
-                                        if (d.getElementById(id))
-                                            return;
-                                        js = d.createElement(s);
-                                        js.id = id;
-                                        js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.6";
-                                        fjs.parentNode.insertBefore(js, fjs);
-                                    }(document, 'script', 'facebook-jssdk'));</script>
+            <div class="col-md-6 portfolio-item">
+                <a href="se devuleve al index">
+                    <img class="img-responsive" src="<?php echo asset("img/ferias.jpg") ?>" alt="">
 
-                                <div class="fb-comments" data-href="https://www.facebook.com/groups/860979787324031/?fref=ts" data-numposts="5"></div>
-      
-                    <?php include ("/../../Templates/Frontend/footer.php") ?>
-                    <?php include ("/../../Templates/Frontend/foot.php") ?>
+
+                </a>
+
+            </div>
+            <div class="botoness">
+                <div class="col-md-2 portfolio-item">
+                    <h3 title="Lugar"class="icon-home fa-fw">:</h3>
+                    <p>Coliseo de ferias</p>
+                </div>
+                <div class="col-md-2 portfolio-item">
+                    <h3 title="Direccion" class="icon-location fa-fw"><b>:</b></h3>
+                    <p>Carrera 8 con calle 21</p>
+                </div>
+                <div class="col-md-2 portfolio-item">
+                    <h3 title="Duraccion" class=" icon-calendar fa-fw"><b>:</b></h3><p>Del 15 de julio hasta el 20 de julio</p>
+                </div>
+                <div class="col-md-2 portfolio-item">
+                    <h3 title="Correo" class=" icon-"><b>:</b></h3>
+                    <p>$30.0000</p>
+                </div>
+                <div class="col-md-2 portfolio-item">
+                    <h3 title="Celular" class="icon-mobile"><b>:</b></h3>
+                    <p>3005647843</p>
+                </div>
+                <div class="col-md-2 portfolio-item">
+                    <h3 title="Hora" class="icon-clock"><b>:</b></h3>
+                    <p>De2pm a 7pm</p>
+                </div>
+                <div class="col-md-3 portfolio-item">
+                    <h3 title="Correo" class=" icon-mail fa-fw"><b>:</b></h3><p>AlcaldiaDeBuga@hotmail.com</p>
+                </div>
+            </div>
+            <div class="col-md-5 col-sm-11 portfolio-item">
+                <h2 style="margin-top: 60px;">Ferias De Buga</h2>
+                <p style="margin-top:20px; "class="text-justify">Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Alex Campos, reconocido en el mundo de la música cristiana.. Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Alex Campos, reconocido en e Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Ale
+                    Los eventos inician el jueves 9 de julio con un gran concierto espiritual con el artista ganador del premio Grammy Al </p>
+            </div>
+            <div id="fb-root"></div>
+            <script>(function (d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id))
+                        return;
+                    js = d.createElement(s);
+                    js.id = id;
+                    js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.6";
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));</script>
+
+            <div class="fb-comments" data-href="https://www.facebook.com/groups/860979787324031/?fref=ts" data-numposts="5"></div>
+
+            <?php include ("/../../Templates/Frontend/footer.php") ?>
+            <?php include ("/../../Templates/Frontend/foot.php") ?>
