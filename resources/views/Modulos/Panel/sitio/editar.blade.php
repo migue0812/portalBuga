6f7358256d874744a0fd31b2a0d6c5abb3d87849
Miguel <Miguel@Miguel-PC>
2016-05-18 10:48:40 -0500

    Modificaciones en CRUD sitios

--- resources/views/Modulos/Panel/sitio/editar.blade.php
+++ resources/views/Modulos/Panel/sitio/editar.blade.php
@@ -85,7 +85,15 @@
         <div class="col-lg-12">
           <div class="form-panel">
             <h4 class="mb"><i class="fa fa-angle-right"></i> Editar Sitio</h4>
-             
+              @if ($errors->any())
+                         <div class="alert alert-danger alert-dismissible center-block" role="alert">
+                             <ul>
+                              @foreach ($errors->all() as $error)  
+                                 <li>{{$error}}</li>
+                                 @endforeach
+                             </ul>
+                         </div>
+                        @endif
             <form class="style-form" method="post" action="{{url('admin/sitio/editar')}}" enctype="multipart/form-data">
               
               <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
