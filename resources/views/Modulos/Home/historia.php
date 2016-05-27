a839c9a4f43ac4492968a3e7d0a19cfb37117440
Luis Angel <Luis Angel@LuisGa>
2016-05-27 12:04:23 -0500

    Historia de buga y sus lugares

--- resources/views/Modulos/Home/historia.php
+++ resources/views/Modulos/Home/historia.php
@@ -53,17 +53,129 @@
             </p>
 
             <div>
-                <h2 style="text-align: center; margin-top: 40px;">Sus Lugares Historicos</h2>
+                <h2 style="text-align: center; margin-top: 60px;">Sus Lugares Historicos</h2>
                 <hr>
 
                 <div class="col-md-6 portfolio-item">
-                   
+
                     <img style="float: right; border-radius: 20px;"  class="img-responsive" src="<?php echo asset("img/c.jpg.jpg") ?>">
+                </div>
+
+
+                <div class="col-md-6 portfolio-item">
+                    <h2 style="text-align: center;">Señor de lo milagros</h2>
+                    <hr>
+
+                    <p style="margin-top: 10px">
+
+                        La palabra Basílica significa “Casa donde vive el Rey”. La iglesia católica le concede el título de “Basílicas” a unos cuantos templos muy celebres de Roma. Ahora en cada país el Santo Padre le concede dicho título a los templos más afanados y preferidos de los fieles.
+
+                        Desde 1875 se promovió la construcción de la Basílica del Señor de los Milagros. Los primeros en dirigirla fueron el superior de la comunidad Redentorista, padre Alfonso París y tres hermanos de esa congregación. Con la bendicion de la primera Piedra el 7 de Agosto de 1892 se inicio la construcción, se requirieron 126 mil arrobas de cal y 4.000.000 de ladrillos. El área total del templo es de 2.088 metros cuadrados. La construcción del templo duro quince años, se hizo en aquellos tiempos difíciles donde nos golpeaban las guerras, por ejemplo, la guerra de los Mil Días. El costo total de laBasílica en 1907 fue de 10 millones de pesos.
+
+                        El Templo mide 80 metros de largo y 33 de altura. Entre las dos hermosas torres hay una estatua del Santísimo Redentorde 2 ½ metros de altura en hierro fundido. El reloj de la torre es de fabricación francesa y da las horas desde el 18 de Marzo de 1909.
+
+                    </p>
+                    <hr>
+
+                </div>
+
+
+                <div>
+                    <h2 style="text-align: center; margin-top: 40px;">PARQUE JOSE MARIA CABAL</h2>
+                    <hr>
+
+                    <div class="col-md-6 portfolio-item">
+
+                        <img style="float: right; border-radius: 20px;"  class="img-responsive" src="<?php echo asset("img/cabal.jpg.jpg") ?>">
+                    </div>
+
+
+                    <div class="col-md-6 portfolio-item">
+
+
+                        <p style="margin-top: 20px">
+
+
+                            La Plaza o Parque principal de la ciudad de Guadalajara de Buga, se encuentra localizada entre la Calle 6a. (Calle de Bolívar) y 7a. (Calle de Santander) y la Carrera 14 (Calle 20 de Julio o Calle del 7 de Agosto o de la Iglesia Mayor).
+
+                            La Plaza Jose María Cabal se inaguró oficialmente el 6 de agosto de 1924. Esta antigua Plaza Mayor, fue diseñada y construida por Don Enrique Figueroa Fernández, a partir de la cual se desarrolló la ciudad de Guadalajara de Buga desde su traslado a este sitio en 1576; su diseño estilo Parque Europeo le da vida a su edificación, en ella se encuentra la estatua en bronce del prócer bugueño General José María Cabal Barona, uno de los mártires de la Independencia y miembro de la Expedición Botánica, obra que realizó el Escultor Fernando Rubinni.
+
+                        <hr>
+                        </p>
+                    </div>
+                    <h2 style="text-align: center; margin-top: 40px;"> CATEDRAL DE SAN PEDRO</h2>
+                    <hr>
+
+                    <div class="col-md-6 portfolio-item">
+
+                        <img style="float: right; border-radius: 20px;"  class="img-responsive" src="<?php echo asset("img/igl.jpg.jpg") ?>">
+                    </div>
+
+
+                    <div class="col-md-6 portfolio-item">
+                    
+
+                        <p style="margin-top: 20px">
+
+
+
+                            ARQUITECTURA COLONIAL
+                            La Catedral de San Pedro, construida en las últimas décadas del siglo XVI, La primera construcción del templo en 1616 fue realizada por los indios y esclavos, con tierra pisada y techo de paja. El terremoto del 9 de julio de 1766 la arruinó en su totalidad. Se habla de una segunda construcción; inicialmente fue regentada por los padres jesuitas hasta el 17 de agosto de 1767 que fueron desterrados por la pragmática de Carlos III; después de estos acontecimientos entro en total abandono.
+
+                            Años después, con la ayuda económica del Rey de España, fue reconstruida; reconstrucción que se dio por terminada el 30 de diciembre de 1781, para luego ser restaurada en el siglo XX. Con la creación de la Diócesis de Buga, fue elevada al rango de Catedral. El templo, en su patio interior, y las paredes de la capilla del Carmen fueron durante 3 siglos el único cementerio de la ciudad en él descansan los restos de los conquistadores y primeros pobladores de Guadalajara de la Victoria de Buga
+
+
+
+                        <hr>
+                        </p>
+                    </div>
+
+                    <h2 style="text-align: center; margin-top: 40px;"> Iglesia santa barbara</h2>
+                    <hr>
+
+                    
+                    <div class="col-md-6 portfolio-item">
+
+                        <img style="float: right; border-radius: 20px;"  class="img-responsive" src="<?php echo asset("img/Bb.jpg.jpg") ?>">
+                    </div>
+
+
+                    <div class="col-md-6 portfolio-item">
                        
-                    <hr class="tagline-divider-cont">
+
+                        <p style="margin-top: 20px">
+
+                            Se construyó hacia el año de 1912. Ubicada en el tradicional y antiguo Barrio Santa Bárbara, en la esquina de la Cra 9a con Calle 13. En vista de la popularidad de la devoción, la Comunidad de Padres Rendentoristas, donó una imagen a la Antigua Capilla de Santa Bárbara; luego los padres trajeron de Francia en 1927, una bella imagen que se encuentra en el Altar Mayor de la Basílica del Señor de los Milagros.
+
+                            En la iglesia Catedral se encuentra un cuadro al oleo de esta santa. SANTA BARBARA. Virgen y mártir de Nicomedia sacrificada en el año 235. Fue educada por afamados maestros, pero su agudo ingenio le descurbrió lo absurdo de la doctrina pagana acerca de la pluralidad de los dioses. Con inmesas dificultades hizo llegar un mensaje al insigne maestro, orígenes de Alejandría, para recibir de él ilustración sobre la doctrina cristiana, la que recibió y llevó a esta doncella a solicitar el Bautismo.
+
+
+                        <hr>
+                        </p>
+                    </div>
+
+                    <h2 style="text-align: center; margin-top: 40px;"> La Estación del Ferrocarril</h2>
+                    <hr>
+                    
+                    <div class="col-md-6 portfolio-item">
+
+                        <img style="float: right; border-radius: 20px;"  class="img-responsive" src="<?php echo asset("img/ferro-jpg.jpg") ?>">
+                    </div>
+
+
+                    <div class="col-md-6 portfolio-item">
                    
-                   
+
+                        <p style="margin-top: 20px">
+                            Desde el final de los años veinte surgió la inquietud de vincular las redes del Pacífico y el centro del país como un propósito nacional. En efecto, en 1929 se dio inicio a la construcción del túnel del cruce de la cordillera en la ruta Ibagué - Armenia, que algunos años después se suspendió ante las dificultades geológicas y las penurias fiscales.
+
+                            Pero el debate del proyecto continuó hasta 1950, en que la misión Currie abocó la evaluación de la infraestructura nacional, como un elemento fundamental del desarrollo económico del país. Para entonces la red del Pacífico contaba con 1.273 kilómetros en trocha de una yarda y la Central disponía de 1.221 kilómetros con 39 % en trocha de un metro y el resto en una yarda. 
+                            La primera recomendación de la misión consistió, pues, en unificar la trocha al ancho preponderante, para permitir una vinculación directa en condiciones económicas. En segundo término, la misión abocó el análisis de las soluciones alternativas para esa vinculación,
+                        <hr>
+                        </p>
+                    </div>
+
+
 
                     <?php include ("/../../Templates/Frontend/footer.php") ?>
                     <?php include ("/../../Templates/Frontend/foot.php") ?>
-        
