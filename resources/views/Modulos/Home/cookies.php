e025cf06207bc1c5c1728c6185f71715023b98fa
Juan Gabo <Juan Gabo@JuanGabo>
2016-05-27 12:20:42 -0500

    vista Politica Cookies y Privacidad (github esta subiendo por partes)

---
+++ resources/views/Modulos/Home/cookies.php
@@ -0,0 +1,34 @@
+<?php include ("/../../Templates/Frontend/head.php") ?>
+<?php include ("/../../Templates/Frontend/header.php") ?>
+
+<div class="container">
+    <div class="row">
+        <div class="box">
+
+            <h2 class="text-center ">Politica de Cookies y Privacidad</h2>
+            <hr>
+            <br>
+            <p class="text-justify">Una cookie es un fichero que se descarga en su ordenador/smartphone/tablet al acceder a determinadas páginas web. Las cookies permiten a una página web, entre otras cosas, almacenar y recuperar información sobre las hábitos de navegación de un usuario o de su equipo y, dependiendo de la información que contenga y de la forma en que utilice su equipo, pueden utilizarse para recononcer al usuario.
+                ¿Qué tipos de cookies utiliza esta página web?
+            </p>
+            <p class="text-justify">
+                Cookies técnicos: Son aquellas creadas y gestionadas únicamente por nosotros, y permiten al usuario la navegación a través de nuestra página web y la utilización de las diferentes opciones como, por ejemplo, identificar la sesión, personalizar información o acceder a zonas de acceso restringido.
+                Cookies de análisis: Son aquellas que, ya sea por nuestra parte o por terceros, permiten cuantificar el número de usuarios y así realizar una medición y análisis estadístico del uso que hacen los usuarios del servicio ofertado. Por ello se analiza navegación en nuestra página web, con el fin de mejorar la oferta de productos o servicios que le ofrecemos.
+                Vista Camacho no tiene cookies publicitarias de terceros.
+            </p>
+
+            <h3 class="text-center">¿Cómo desactivar cookies?</h3>
+            <p class="text-justify">
+                Puede permitir, bloquear o eliminar las cookies instaladas en su equipo mediante la configuración de las opciones del navegador instalado en su ordenador. </p>
+            <p>
+            </p>
+            <p>
+                <i>
+                    La información almacenada en las cookies de nuestro sitio web es utilizada exclusivamente con fines estadísticos.
+                </i>
+            </p>
+
+
+            <?php include ("/../../Templates/Frontend/footer.php") ?>
+            <?php include ("/../../Templates/Frontend/foot.php") ?>
+        
