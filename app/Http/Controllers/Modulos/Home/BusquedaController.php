ae15dc7ae7c4eaf0e546966af1034108d26a1754
EstebanCuadros <EstebanCuadros@Esteban>
2016-06-15 17:48:25 -0500

    Arreglo de busqueda 1

--- app/Http/Controllers/Modulos/Home/BusquedaController.php
+++ app/Http/Controllers/Modulos/Home/BusquedaController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers\Modulos\Home;
+
 use App\Http\Requests;
 use DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Session;
+
 /**
  * Description of BusquedaController
  *
@@ -15,23 +17,20 @@ class BusquedaController extends Controller {
     function getIndex() {
         return view('Modulos.Home.busqueda');
     }
-    
-    function postBuscar(){
-        $nombre=$_POST['nombre'];
-            $resultados = DB::select("SELECT * FROM `bdp_sitio`,`bdp_imagen` WHERE bdp_sitio.`sit_id`=bdp_imagen.`sit_id` and (`sit_nombre` LIKE '%$nombre%' or `sit_descripcion` LIKE '%$nombre%')");
-            $eventos= DB::select("SELECT * FROM `bdp_evento`,`bdp_imagen` WHERE bdp_evento.`eve_id`=bdp_imagen.`eve_id` and (`eve_nombre` LIKE '%$nombre%' or `eve_descripcion` LIKE '%$nombre%')");
-    	
-        
-         
-//        if (isset($resultados,$eventos)){
-//            Session::flash("Resultados", "No se encontraron resultados");
-//            return view('Modulos.Home.busqueda', compact("resultados"));
-//        }else{
-//            return view('Modulos.Home.busqueda', compact("resultados"));
-//        }
-            return view('Modulos.Home.busqueda', compact("resultados"),compact("eventos"));
-        
-        
+
+    function postBuscar() {
+        $nombre = $_POST['nombre'];
+        $resultados = DB::select("SELECT distinct bdp_imagen.`sit_id`,bdp_imagen.`img_ruta`,bdp_sitio.`sit_nombre` FROM `bdp_sitio`,`bdp_imagen` WHERE bdp_sitio.`sit_id`=bdp_imagen.`sit_id` and (`sit_nombre` LIKE '%$nombre%' or `sit_descripcion` LIKE '%$nombre%') GROUP BY bdp_imagen.`sit_id` ");
+        $eventos = DB::select("SELECT * FROM `bdp_evento`,`bdp_imagen` WHERE bdp_evento.`eve_id`=bdp_imagen.`eve_id` and (`eve_nombre` LIKE '%$nombre%' or `eve_descripcion` LIKE '%$nombre%')");
+
+
+        if (empty($resultados)) {
+            Session::flash("Resultados", "No se encontraron resultados");
+        }
+        if (empty($eventos)) {
+            Session::flash("Eventos", "No se encontraron resultados");
+        }
+        return view('Modulos.Home.busqueda', compact("resultados"), compact("eventos"));
     }
 
 }
--- resources/views/Modulos/Home/busqueda.blade.php
+++ resources/views/Modulos/Home/busqueda.blade.php
@@ -12,19 +12,23 @@
                     </div>
                 </div>
                 <hr class="tagline-divider-cont">
+                <?php if (Session::has("Resultados")): ?>
+                    <?php echo "<h3><center>".Session::get("Resultados")."</center></h3>" ?>
+                <?php endif ?>
             </div>
             <!-- /.row -->
 
             <!-- Projects Row -->
             <div class="row">
+
                 <!-- Categoria 1-->
                 <?php foreach ($resultados as $resultado): ?>
                     <div class="col-md-4 portfolio-item">
-                        <a href="#">
+                        <a href="<?php echo url("home/sitios/det/" . $resultado->sit_id) ?>">
                             <img class="img-responsive" src="<?php echo asset("$resultado->img_ruta") ?>" alt="<?php echo ((strlen($resultado->sit_nombre) > 15) ? substr(($resultado->sit_nombre), 0, 15) . " ..." : ($resultado->sit_nombre)) ?>">
                         </a>
                         <h3>
-                            <a href="#" ><?php echo ((strlen($resultado->sit_nombre) > 15) ? substr(($resultado->sit_nombre), 0, 15) . " ..." : ($resultado->sit_nombre)) ?></a>
+                            <?php echo ((strlen($resultado->sit_nombre) > 15) ? substr(($resultado->sit_nombre), 0, 15) . " ..." : ($resultado->sit_nombre)) ?>
                         </h3>
                         <div class="contenedor-botones">
                             <a href="<?php echo url("home/sitios/det/" . $resultado->sit_id) ?>" class="btn btn-primary">Ver Mas</a>
@@ -40,16 +44,22 @@
                         <div>
                             <h2 class="text-center ">Eventos</h2>
                         </div>
+
                     </div>
                     <hr class="tagline-divider-cont">
+                    <?php if (Session::has("Eventos")): ?>
+                        <?php echo "<h3><center>".Session::get("Eventos")."</center></h3>" ?>
+                    <?php endif ?>
                 </div>
+
+
                 <?php foreach ($eventos as $evento): ?>
                     <div class="col-md-4 portfolio-item">
                         <a href="#">
                             <img class="img-responsive" src="<?php echo asset("$evento->img_ruta") ?>" alt="">
                         </a>
                         <h3>
-                            <a href="#" ><?php echo ((strlen($resultado->sit_nombre) > 15) ? substr(($resultado->sit_nombre), 0, 15) . " ..." : ($resultado->sit_nombre)) ?></a>
+                            <?php echo ((strlen($evento->eve_nombre) > 15) ? substr(($evento->eve_nombre), 0, 15) . " ..." : ($evento->eve_nombre)) ?>
                         </h3>
                         <div class="contenedor-botones">
                             <a href="<?php echo url("home/evento/det/" . $evento->eve_id) ?>" class="btn btn-primary">Ver Mas</a>
