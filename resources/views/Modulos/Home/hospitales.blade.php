b7c90ad7dc5058940f290811c95a60a8e27adf6d
Luis Angel <Luis Angel@LuisGa>
2016-06-21 10:34:35 -0500

    modificaciones

--- resources/views/Modulos/Home/hospitales.blade.php
+++ resources/views/Modulos/Home/hospitales.blade.php
@@ -7,7 +7,7 @@
         <div class="box">
 
 
-        <div class="col-lg-12">
+            <div class="col-lg-12">
                 <div class="visitNav">
                     <div>
                         <h2 class="text-center">Clínicas Y Hospitales De Guadalajara De Buga</h2>
@@ -30,174 +30,216 @@
                 <div class="col-md-7 portfolio-item">
                     <img class="img-responsive border-img" src="<?php echo asset("img/elsanjose.jpg") ?>">
                 </div>
+
+
                 <div class="col-md-5">
-                    <ul>
-                        <li  style="font: 150% sans-serif;">Hospital San Jose</li>
-                        <br>
-                        <br>
-                        <li style="font: 150% sans-serif;">Direccion: Carrera 8 N° 17-52.</li>
-                        <br>
-                        <br>
-                        <li style="font: 150% sans-serif;">Nivel: III</li>
-                        <br>
-                        <br>
-                        <li style="font: 150% sans-serif;">Telefono: 2361000</li>
-                        <br>
-                        <br>
-                        <li style="font: 150% sans-serif;">Correo: auditoria@fhsjb.org</li>
-                    </ul>
+
+                    <div class="panel-heading">
+                        <strong>Datos</strong>
+                    </div>
+                    <div class="panel-body">
+                        <div class="media">
+
+                            <div class="media-body">
+                                <h4 class="media-heading">DIRECCION:</h4>
+                                <p>CALLE 6 No. 16 – 21 Buga</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <div class="media">
+
+                            <div class="media-body">
+                                <h4 class="media-heading">TELEFONO:</h4>
+                                <p>316 618 7865 - 316 5329690</p>							
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <div class="media">
+
+                            <div class="media-body">
+                                <h4 class="media-heading">CORREO:</h4>
+                                <p></p>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
                 <div>
                     <p  class="col-md-10" style="margin-top: 30px;  text-align: justify">
                         La Fundación Hospital San José de Buga es una institución dedicada a la prestación de servicios de salud de carácter Fundacional sin ánimo de lucro y de origen privado, de beneficio social en el sector de la salud, reglamentada por el Sistema General de Seguridad Social Integral en Salud de Colombia y sus propios Estatutos. Su sede de operación se encuentra ubicada en el centro departamento del Valle del Cauca en el norte de la ciudad de Guadalajara de Buga, en la carrera 8 N° 17-52.</p>
                 </div>
             </div>
-                <div class="col-lg-12">
-                    <div class="visitNav">
-                        <div>
-                            <h2 class="text-center">Urgencias Medicas</h2>
-                        </div>
-                    </div>
-                    <hr class="tagline-divider-cont">
-                </div>
-
-            
-
-                <div class="container portImg">
-                    <div class="col-md-7 portfolio-item">
-                        <img class="img-responsive border-img" src="<?php echo asset("img/urgencias.jpg") ?>">
-                    </div>
-                    <div class="col-md-5">
-                        <ul>
-                            <li  style="font: 150% sans-serif;">Urgencias Medicas</li>
-                            <br>
-                            <br>
-                            <li style="font: 150% sans-serif;">Direccion: Calle 4 N° 22- 10 </li>
-                            <br>
-                            <br>
-                            <li style="font: 150% sans-serif;">Nivel: III</li>
-                            <br>
-                            <br>
-                            <li style="font: 150% sans-serif;">Telefono: 2282246</li>
-                            <br>
-                            <br>
-                            <li style="font: 150% sans-serif;">Correo:urmedicas@ert.com.co</li>
-                        </ul>
-                    </div>
+            <div class="col-lg-12">
+                <div class="visitNav">
                     <div>
-                        <p  class="col-md-10" style="margin-top: 30px;  text-align: justify">Pediatría, Laboratorío Clínico, Otorrinolaringología, Rayos, Traumatología, Cirugía Estética, Escanografía Helicoidal,Cirugía Ambulatoria.
-                            Urgencias Médicas es una Institución con 25 años de trayectoria y amplio reconocimiento adquirido con base a responsabilidad, cumplimiento, trabajo profesional y excelente servicio. 
-
-                            Contamos en este momento con una sede ubicada en la carrera 16 7-68 punto estratégico y central de la ciudad. </p>
+                        <h2 class="text-center">Urgencias Medicas</h2>
                     </div>
                 </div>
+                <hr class="tagline-divider-cont">
+            </div>
 
-                    <div class="col-lg-12">
-                        <div class="visitNav">
-                            <div>
-                                <h2 class="text-center">Clinica Guadalajara</h2>
-                            </div>
-                        </div>
-                        <hr class="tagline-divider-cont">
+
+
+            <div class="container portImg">
+                <div class="col-md-7 portfolio-item">
+                    <img class="img-responsive border-img" src="<?php echo asset("img/urgencias.jpg") ?>">
+                </div>
+
+
+                <div class="col-md-5">
+
+                    <div class="panel-heading">
+                        <strong>Datos</strong>
                     </div>
+                    <div class="panel-body">
+                        <div class="media">
 
-
-                    <div class="container portImg">
-                        <div class="col-md-7 portfolio-item">
-                            <img class="img-responsive border-img" src="<?php echo asset("img/clinicaguadalajara.JPG") ?>">
-                        </div>
-                        <div class="col-md-5">
-                            <ul>
-                                <li  style="font: 150% sans-serif;">Clinica Guadalajara</li>
-                                <br>
-                                <br>
-                                <li style="font: 150% sans-serif;">Direccion: Calle 1 N° 15-28.</li>
-                                <br>
-                                <br>
-                                <li style="font: 150% sans-serif;">Nivel: II</li>
-                                <br>
-                                <br>
-                                <li style="font: 150% sans-serif;">Telefono: 2270007</li>
-                                <br>
-                                <br>
-                                <li style="font: 150% sans-serif;">Correo:  cgbco@uniweb.net.co</li>
-                            </ul>
-                        </div>
-                        <div>
-                            <p  class="col-md-10" style="margin-top: 30px;  text-align: justify">
-                                Apoyo Diagnóstico y Complementación Terapéutica, Consulta Externa, Hospitalario, Promoción y Prevención, Quirúrgico, Urgencias. </p>
+                            <div class="media-body">
+                                <h4 class="media-heading">DIRECCION:</h4>
+                                <p>CALLE 6 No. 16 – 21 Buga</p>
+                            </div>
                         </div>
                     </div>
-                        <div class="col-lg-12">
-                            <div class="visitNav">
-                                <div>
-                                    <h2 class="text-center">Clinica La Merced</h2>
-                                </div>
+                    <div class="panel-body">
+                        <div class="media">
+
+                            <div class="media-body">
+                                <h4 class="media-heading">TELEFONO:</h4>
+                                <p>316 618 7865 - 316 5329690</p>							
                             </div>
-                            <hr class="tagline-divider-cont">
                         </div>
+                    </div>
+                    <div class="panel-body">
+                        <div class="media">
 
-
-                        <div class="container portImg">
-                            <div class="col-md-7 portfolio-item">
-                                <img class="img-responsive border-img" src="<?php echo asset("img/thumb.jpg") ?>">
+                            <div class="media-body">
+                                <h4 class="media-heading">CORREO:</h4>
+                                <p></p>
                             </div>
-                            <div class="col-md-5">
-                                <ul>
-                                    <li  style="font: 150% sans-serif;">Clinica La Merced</li>
-                                    <br>
-                                    <br>
-                                    <li style="font: 150% sans-serif;">Direccion: Calle 1 No. 14 - 34.</li>
-                                    <br>
-                                    <br>
-                                    <li style="font: 150% sans-serif;">Nivel: III</li>
-                                    <br>
-                                    <br>
-                                    <li style="font: 150% sans-serif;">Telefono: 2282744</li>
-                                    <br>
-                                    <br>
-                                    <li style="font: 150% sans-serif;">Correo:</li>
-                                </ul>
+                        </div>
+                    </div>
+
+                </div>
+                <div>
+                    <p  class="col-md-10" style="margin-top: 30px;  text-align: justify">Pediatría, Laboratorío Clínico, Otorrinolaringología, Rayos, Traumatología, Cirugía Estética, Escanografía Helicoidal,Cirugía Ambulatoria.
+                        Urgencias Médicas es una Institución con 25 años de trayectoria y amplio reconocimiento adquirido con base a responsabilidad, cumplimiento, trabajo profesional y excelente servicio. 
+
+                        Contamos en este momento con una sede ubicada en la carrera 16 7-68 punto estratégico y central de la ciudad. </p>
+                </div>
+            </div>
+
+            <div class="col-lg-12">
+                <div class="visitNav">
+                    <div>
+                        <h2 class="text-center">Clinica Guadalajara</h2>
+                    </div>
+                </div>
+                <hr class="tagline-divider-cont">
+            </div>
+
+
+            <div class="container portImg">
+                <div class="col-md-7 portfolio-item">
+                    <img class="img-responsive border-img" src="<?php echo asset("img/clinicaguadalajara.JPG") ?>">
+                </div>
+
+
+                <div class="col-md-5">
+
+                    <div class="panel-heading">
+                        <strong>Datos</strong>
+                    </div>
+                    <div class="panel-body">
+                        <div class="media">
+
+                            <div class="media-body">
+                                <h4 class="media-heading">DIRECCION:</h4>
+                                <p>CALLE 6 No. 16 – 21 Buga</p>
                             </div>
-                         
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <div class="media">
 
+                            <div class="media-body">
+                                <h4 class="media-heading">TELEFONO:</h4>
+                                <p>316 618 7865 - 316 5329690</p>							
                             </div>
-                            <div class="col-lg-12">
-                                <div class="visitNav">
-                                    <div>
-                                        <h2 class="text-center"> Centro De Salud Comfamdi</h2>
-                                    </div>
-                                </div>
-                                <hr class="tagline-divider-cont">
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <div class="media">
+
+                            <div class="media-body">
+                                <h4 class="media-heading">CORREO:</h4>
+                                <p></p>
                             </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div>
+                    <p  class="col-md-10" style="margin-top: 30px;  text-align: justify">
+                        Apoyo Diagnóstico y Complementación Terapéutica, Consulta Externa, Hospitalario, Promoción y Prevención, Quirúrgico, Urgencias. </p>
+                </div>
+            </div>
+            <div class="col-lg-12">
+                <div class="visitNav">
+                    <div>
+                        <h2 class="text-center">Clinica La Merced</h2>
+                    </div>
+                </div>
+                <hr class="tagline-divider-cont">
+            </div>
 
 
+            <div class="container portImg">
+                <div class="col-md-7 portfolio-item">
+                    <img class="img-responsive border-img" src="<?php echo asset("img/thumb.jpg") ?>">
+                </div>
 
-                            <div class="container portImg">
-                                <div class="col-md-7 portfolio-item">
-                                    <img class="img-responsive border-img" src="<?php echo asset("img/2_1.jpg") ?>">
-                                </div>
-                                <div class="col-md-5">
-                                    <ul>
-                                        <li  style="font: 150% sans-serif;">Comfandi</li>
-                                        <br>
-                                        <br>
-                                        <li style="font: 150% sans-serif;">Direccion: Carrera 17 # 5 - 18</li>
-                                        <br>
-                                        <br>
-                                        <li style="font: 150% sans-serif;">Nivel: III</li>
-                                        <br>
-                                        <br>
-                                        <li style="font: 150% sans-serif;">Telefono:  2367445</li>
-                                        <br>
-                                        <br>
-                                        <li style="font: 150% sans-serif;">Correo:  </li>
-                                    </ul>
-                                </div>
-                                <div>
-                                    <p  class="col-md-10" style="margin-top: 30px;  text-align: justify">La IPS Comfandi Buga se encuentra ubicada en un sitio estratégico de la ciudad, cerca a la Basílica del Señor de los Milagros. Cuenta con una infraestructura exclusiva y especialmente diseñada para brindar servicios de salud, con amplias y cómodas instalaciones. </p>
-                                </div>
+
+                <div class="col-md-5">
+
+                    <div class="panel-heading">
+                        <strong>Datos</strong>
+                    </div>
+                    <div class="panel-body">
+                        <div class="media">
+
+                            <div class="media-body">
+                                <h4 class="media-heading">DIRECCION:</h4>
+                                <p>CALLE 6 No. 16 – 21 Buga</p>
                             </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <div class="media">
 
-                                @include('Templates.Frontend.footer')
-                                @include('Templates.Frontend.foot')
+                            <div class="media-body">
+                                <h4 class="media-heading">TELEFONO:</h4>
+                                <p>316 618 7865 - 316 5329690</p>							
+                            </div>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <div class="media">
+
+                            <div class="media-body">
+                                <h4 class="media-heading">CORREO:</h4>
+                                <p></p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div>
+                    <p  class="col-md-10" style="margin-top: 30px;  text-align: justify">
+                   La Clínica cuenta con el personal idóneo, tanto asistencial como administrativo con la suficiente experticia para el manejo de las diferentes actividades en pro del paciente crítico. Contamos con Médicos internistas, Infecologos, Cirujanos, Anestesiólogos, Terapeutas, Fisioterapeutas, Enfermeras con especialidad en cuidado crítico, Auxiliares.</p>
+                </div>
+            </div>
+
+            @include('Templates.Frontend.footer')
+            @include('Templates.Frontend.foot')
