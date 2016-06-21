823f8756cec6d0bfe29f40d30e644de25267f436
David <David@Davidim>
2016-06-21 10:54:48 -0500

    modificacion texto vista sitios sin terminar

--- resources/views/Modulos/Home/sitios.blade.php
+++ resources/views/Modulos/Home/sitios.blade.php
@@ -4,31 +4,40 @@
 <div class="container">
   <div class="row">
     <div class="box ">
-      <div class="visitNav">
-        <h2 class="text-center">SITIOS TURISTICOS DE BUGA</h2>
+      
+      <div class="col-lg-12">
+        <div class="visitNav">
+          <div>
+            <h2 class="text-center">SITIOS TURISTICOS</h2>
+          </div>
+        </div>
+        <hr class="tagline-divider-cont">
       </div>
 
-      <hr class="tagline-divider-cont">
       <?php foreach ($sitios as $sitio): ?>
         <div class="container portImg">
-          <div class="row ">
-
+          <div class="row">
+            
             <div class="col-md-6 portfolio-item">
               <a href="<?php echo url("home/sitios/det/" . $sitio->sit_id) ?>">
                 <img class="img-contenido" src="<?php echo asset("$sitio->img_ruta") ?>" alt="">
               </a>
-
             </div>
 
 
             <div class="col-md-5 portfolio-item">
-              <h2 style="text-align: center"><?php echo ($sitio->sit_nombre) ?></h2>
+              <h2 class="text-center"><?php echo ($sitio->sit_nombre) ?></h2>
               <hr class="tagline-divider-cont">
-              <br>
-              <p class="text-justify"><?php echo ((strlen($sitio->sit_descripcion) > 200) ? substr(($sitio->sit_descripcion), 0, 200) . " ..." : ($sitio->sit_descripcion)) ?></p>
-              <a href="<?php echo url("home/sitios/det/" . $sitio->sit_id) ?>" class="btn btn-primary"><b title="Ver mas"class="fa fa-eye"></b></a>
+              <p class="text-justify">
+                <?php echo ((strlen($sitio->sit_descripcion) > 200) ? substr(($sitio->sit_descripcion), 0, 200) . " ..." : ($sitio->sit_descripcion)) ?>
+              </p>
+              <a href="<?php echo url("home/sitios/det/" . $sitio->sit_id) ?>" class="btn btn-primary">
+                <b title="Ver mas"> Ver Mas</b>
+              </a>
               <?php if (Session::has("usuarioLogueado")): ?>
-                <a href="<?php echo url("itinerario/sitio/" . $sitio->sit_id) ?>" class="btn btn-primary"><b title="Itinerario" class="icon-direction fa-fw"></b></a>
+                <a href="<?php echo url("itinerario/sitio/" . $sitio->sit_id) ?>" class="btn btn-primary">
+                  <b title="Itinerario" class="icon-direction fa-fw"></b>
+                </a>
               <?php endif ?>
             </div>
           </div>
@@ -50,19 +59,19 @@
             ?>
 
             <div class="col-md-4 portfolio-item">
-                <div class="imgSitios">
-              <div class="contenedor-imgX fxX"> 
-                <div id="tituloalz"><h2><?php echo ($sitio->sit_nombre) ?></h2></div>
-                <a href="<?php echo url("home/sitios/det/" . $sitio->sit_id) ?>">
-                <div class="imgback img-contenido" style=" background-image: url('<?php echo asset("$sitio->img_ruta") ?>');"> </div> </a>
-                <p><?php echo ((strlen($sitio->sit_descripcion) > 100) ? substr(($sitio->sit_descripcion), 0, 100) . " ..." : ($sitio->sit_descripcion)) ?></p>
-                <a href="<?php echo url("home/sitios/det/" . $sitio->sit_id) ?>" class="btn btn-primary"><b title="Ver mas"class="fa fa-eye"></b></a>
-                <?php if (Session::has("usuarioLogueado")): ?>
-                  <a href="<?php echo url("itinerario/sitio/" . $sitio->sit_id) ?>" class="btn btn-primary"><b title="Itinerario" class="icon-direction fa-fw"></b></a>
-                <?php endif ?>
-                 
+              <div class="imgSitios">
+                <div class="contenedor-imgX fxX"> 
+                  <div id="tituloalz"><h2><?php echo ($sitio->sit_nombre) ?></h2></div>
+                  <a href="<?php echo url("home/sitios/det/" . $sitio->sit_id) ?>">
+                    <div class="imgback img-contenido" style=" background-image: url('<?php echo asset("$sitio->img_ruta") ?>');"> </div> </a>
+                  <p><?php echo ((strlen($sitio->sit_descripcion) > 100) ? substr(($sitio->sit_descripcion), 0, 100) . " ..." : ($sitio->sit_descripcion)) ?></p>
+                  <a href="<?php echo url("home/sitios/det/" . $sitio->sit_id) ?>" class="btn btn-primary"><b title="Ver mas"class="fa fa-eye"></b></a>
+                  <?php if (Session::has("usuarioLogueado")): ?>
+                    <a href="<?php echo url("itinerario/sitio/" . $sitio->sit_id) ?>" class="btn btn-primary"><b title="Itinerario" class="icon-direction fa-fw"></b></a>
+                    <?php endif ?>
+
+                </div>
               </div>
-            </div>
             </div>
 
             <?php
@@ -71,7 +80,7 @@
         </div>
       </div>
 
-      
-        @include('Templates.Frontend.footer')
-        @include('Templates.Frontend.foot')
-   
+
+      @include('Templates.Frontend.footer')
+      @include('Templates.Frontend.foot')
+
